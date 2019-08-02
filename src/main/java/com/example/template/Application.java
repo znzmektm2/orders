196a8a7385410f692785a4cadd708c1271104d9b
kosta <kosta@192.168.0.134>
2019-08-02 11:20:03 +0900

    진슬수정

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -3,6 +3,11 @@ package com.example.template;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Application {
@@ -10,7 +15,19 @@ public class Application {
     protected static ApplicationContext applicationContext;
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(Application.class, args);
+        
+        OrderService orderService = applicationContext.getBean(OrderService.class);
+        orderService.getProducts();
     }
+    
+    @Bean
+	RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setObjectMapper(new ObjectMapper());
+		restTemplate.getMessageConverters().add(converter);
+		return restTemplate;
+	}
 
 }
 
--- src/main/java/com/example/template/OrderService.java
+++ src/main/java/com/example/template/OrderService.java
@@ -1,74 +1,49 @@
 package com.example.template;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
-	//@Autowired
-//	private KafkaTemplate kafkaTemplate;
+	@Autowired
+	private KafkaTemplate kafkaTemplate;
 
-	//@Autowired
-	//private OrderRepository orderRepository;
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired(required = true)
+	RestTemplate restTemplate;
 
 	/**
 	 * 상품 가격 가져오기
 	 */
 
-	/**
-     * 재고여부 확인
-     */
-//    @KafkaListener(topics = "eventTopic")
-    //public void checkStock(@Payload String message, ConsumerRecord<?, ?> consumerRecord) {
-  //  	System.out.println("##### listener : " + message);
-
-  //      ObjectMapper objectMapper = new ObjectMapper();
-        //objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        /*ProductRequired productRequired = null;
-        try {
-            productRequired = objectMapper.readValue(message, ProductRequired.class);
-
-            *//**
-             * 상품 추가 요청이 왔을때 해당 상품을 찾아서 재고를 늘린다.
-             *//*
-            if( productRequired.getType().equals(ProductRequired.class.getSimpleName())){
-
-                List<Product> productList = productRepository.findByName(productRequired.getProductName());
-                Product product = null;
-                if( productList != null && productList.size() > 0 ){
-                    product = productList.get(0);
-                }
-
-                if( product == null ) {
-                    product = new Product();
-                    product.setName(productRequired.getProductName());
-                    product.setPrice(10000);
-                    product.setStock(1);
-                }
-
-                // product 의 수량을 10개씩 늘린다
-                product.setStock(product.getStock() + 10);
-
-                productRepository.save(product);
-
-            }
-
-        }catch(
-
-	Exception e)
+	
+	public void getProducts()
 	{
-*/
+	     
+	    final String baseUrl = "http://192.168.0.116:8085/products/1";
+	     
+	    HttpHeaders headers = new HttpHeaders();
+
+	    Product product = restTemplate.getForObject(baseUrl, Product.class);
+	    System.out.println(product.getPrice());
+	    
 	}
+}
+	
+	
+	
+
 
